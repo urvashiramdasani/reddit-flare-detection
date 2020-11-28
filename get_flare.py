8d7f6fc87c79cf03b9ba322820352c400e92e837
urvashiramdasani <54858768+urvashiramdasani@users.noreply.github.com>
2020-11-28 13:07:46 +0530

    get_flare

--- get_flare.py
+++ get_flare.py
@@ -2,7 +2,14 @@
 
 from flask import Flask, render_template, request, redirect, url_for
 import pickle
-from get_tweets import get_related_tweets
+import requests
+from bs4 import BeautifulSoup
+import nltk
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer
+import os.path
+import joblib
 
 # load the pipeline object
 
@@ -10,5 +17,25 @@ pipeline = pickle.load("model.pkl")
 
 # function to get results for a particular text query
 
-def requestResults(link):
-	
+def requestResults(url):
+	response = requests.get(url)
+	soup = BeautifulSoup(response.text, 'lxml')
+	pretified_page = soup.prettify()
+	td = soup.select('h1')
+	title = td[0].text
+	title = title.lower()
+	title = title.split()
+	ps = PorterStemmer()
+	title = [ps.stem(word) for word in title if not word in set(stopwords.words('english'))]
+	title = ' '.join(title)
+	# Load vectorizer
+	vectorizer = joblib.load("vectorizer.joblib")
+	x = vectorizer.transform([title])
+
+	nb = pickle.load("model.pkl")
+	predicted_flare = nb.predict(x)
+	print(predicted_flare)
+
+	flares = []
+
+requestResults("https://www.reddit.com/r/india/comments/jj8aog/coronavirus_covid19_megathread_news_and_updates_7/")
